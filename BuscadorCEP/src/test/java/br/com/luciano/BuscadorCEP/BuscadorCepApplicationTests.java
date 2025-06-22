fbea0311ef9c90ef82ac152f31ca9e1609fdd83c
Luciano Costa <lucianocostax@gmail.com>
2025-06-22 15:42:21 -0300

    Alteração do Layout da página

---
+++ BuscadorCEP/src/test/java/br/com/luciano/BuscadorCEP/BuscadorCepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luciano.BuscadorCEP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscadorCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
